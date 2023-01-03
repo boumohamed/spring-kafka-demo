5ef2be7beb6b5e551938a72bf4ed3d2e437408be
boumohamed <mb.bouzri@gmail.com>
2023-01-03 21:31:58 +0100

    demo : :bulb:

--- src/main/java/me/bouzri/springcloudekafkademo/services/PageEventService.java
+++ src/main/java/me/bouzri/springcloudekafkademo/services/PageEventService.java
@@ -4,7 +4,10 @@ import me.bouzri.springcloudekafkademo.entities.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @Service
 public class PageEventService {
@@ -13,8 +16,13 @@ public class PageEventService {
     {
         return (input) -> {
             System.out.println("--------------------------");
-            System.out.println(input);
+            System.out.println(input.toString());
             System.out.println("--------------------------");
         };
+    }
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier()
+    {
+        return () ->  new PageEvent(Math.random() > 0.5 ? "www.bouzri.com" : "www.asri.fr", Math.random() > 0.5 ? "Bouzri" : "Asri", new Date(), new Random().nextInt(5000));
     }
 }
